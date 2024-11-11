b0c2db31621e5ddc39ec4b5c10f951607efbe301
toukirraju <toukirraju@gmail.com>
2024-11-12 01:12:55 +0600

    fix:set default theme

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    // domains: [
-    //   "cdn.tuk.dev",
-    //   "images.unsplash.com",
-    //   "plus.unsplash.com",
-    //   "cdn.dribbble.com",
-    //   "cdn.pixabay.com",
-    //   "i.pravatar.cc",
-    //   "via.placeholder.com",
-    // ],
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.tuk.dev' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
